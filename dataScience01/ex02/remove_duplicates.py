d746868a6a338597e99ccb27e8ae8f9d618714f2
Sian Soon yee <ssian@ssian.42singapore.sutd.edu.sg>
2025-09-23 16:29:23 +0800

    add ex02

--- dataScience01/ex02/remove_duplicates.py
+++ dataScience01/ex02/remove_duplicates.py
@@ -3,7 +3,7 @@ from psycopg2 import sql, connect
 
 def get_db_conn():
     """Establishes and returns a connection to the PostgreSQL database."""
-
+    
     conn = connect(
         host="localhost",
         port=5432,
@@ -14,7 +14,7 @@ def get_db_conn():
     return conn
 
 
-def union_tables(cur, schema, new_table, tables):
+'''def union_tables(cur, schema, new_table, tables):
     """Creates a new table by performing a UNION of a list of tables."""
 
     union_tables_sql = f"CREATE TABLE {schema}.{new_table} AS"
@@ -25,7 +25,10 @@ def union_tables(cur, schema, new_table, tables):
         else:
             union_tables_sql += f" UNION ALL SELECT * FROM {schema}.{tables[i]}"
     
-    cur.execute(union_tables_sql)
+    cur.execute(union_tables_sql)'''
+def remove_duplicates(cur, schema, table):
+    
+
 
 def main():
     """main()"""
@@ -35,9 +38,9 @@ def main():
         cur = conn.cursor()
 
         schema = "customer2"
-        new_table = "customers"
-        tables = ["data_2022_oct", "data_2022_nov", "data_2022_dec", "data_2023_jan", "data_2023_feb"]
-        union_tables(cur, schema, new_table, tables)
+        table = "customers"
+        #tables = ["data_2022_oct", "data_2022_nov", "data_2022_dec", "data_2023_jan", "data_2023_feb"]
+        remove_duplicates(cur, schema, table)
 
         conn.commit()
         cur.close()
@@ -48,3 +51,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+DELETE FROM customer2.customers 
+WHERE ctid IN (
+    SELECT c1.ctid
+    FROM customer2.customers c1
+    INNER JOIN customer2.customers c2 ON 
+        c1.event_type = c2.event_type 
+        AND c1.product_id = c2.product_id
+        AND c1.user_id = c2.user_id
+        AND c1.user_session = c2.user_session
+        AND c1.price = c2.price
+        AND ABS(EXTRACT(EPOCH FROM (c1.event_time - c2.event_time))) <= 1
+		OR c1.event_time = c2.event_time
+        AND c1.ctid > c2.ctid  -- Keep the earliest record
+);
+"""
